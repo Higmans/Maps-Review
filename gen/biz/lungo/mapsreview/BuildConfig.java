ad995de54e1885c7e869e4a53b1ee32db0450989
lungo <lungo@lungo-PC>
2014-01-21 15:07:48 +0200

    First commit

---
+++ gen/biz/lungo/mapsreview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package biz.lungo.mapsreview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/biz/lungo/mapsreview/MainActivity.java
@@ -0,0 +1,67 @@
+package biz.lungo.mapsreview;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+	double latitude = 50.7;
+	double longitude = 30.7;
+	 
+    // Google Map
+    private GoogleMap googleMap;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	android.support.v4.app.Fragment f = getSupportFragmentManager().findFragmentById(R.id.map);
+        if (f != null) 
+            getSupportFragmentManager().beginTransaction().remove(f).commit();
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+            }
+        }
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+         
+        // adding marker
+        googleMap.addMarker(marker);
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+    
+ 
+}
