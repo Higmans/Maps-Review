58353b98b422277ea1f9f6d0a01b755d2642c4ca
lungo <lungo@lungo-PC>
2014-01-28 16:51:00 +0200

    Multiple markers and PolyLine

--- src/biz/lungo/mapsreview/MainActivity.java
+++ src/biz/lungo/mapsreview/MainActivity.java
@@ -1,23 +1,27 @@
 package biz.lungo.mapsreview;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.CameraPosition.Builder;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import android.annotation.SuppressLint;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
@@ -32,7 +36,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements OnMapClickListener {
+public class MainActivity extends FragmentActivity implements OnMapClickListener, OnMapLongClickListener {
 	public static String CLEAN;
 	public static String PICK_AREA;
 	public static String MAP_TYPE;
@@ -46,6 +50,10 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 	double longitude = 30.7;
 	FragmentManager manager = getFragmentManager();
 	TextView textInfo;
+	List<LatLng> llList;
+	int markerCounter = 0;
+	String markerNames[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+	int maxMarkersCount = markerNames.length;
 
 	// Google Map
 	private GoogleMap googleMap;
@@ -64,6 +72,7 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 		physical = getResources().getString(R.string.physical);
 		none = getResources().getString(R.string.none);
 		textInfo = (TextView) findViewById(R.id.textViewInfo);
+		llList = new ArrayList<LatLng>();
 		try {
 			// Loading map
 			initilizeMap();
@@ -80,6 +89,7 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 		CameraUpdate cu = CameraUpdateFactory.newCameraPosition(cp);
 		googleMap.animateCamera(cu);
 		googleMap.setOnMapClickListener(this);
+		googleMap.setOnMapLongClickListener(this);
 
 	}
 
@@ -106,12 +116,6 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 						.show();
 			}
 		}
-		// create marker
-		MarkerOptions marker = new MarkerOptions().position(
-				new LatLng(latitude, longitude)).title("Hello Maps");
-
-		// adding marker
-		googleMap.addMarker(marker);
 	}
 
 	@Override
@@ -139,11 +143,12 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 		} else if (PICK_AREA.equals(item.getTitle())) {
 			Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
 		} else if (CLEAN.equals(item.getTitle())) {
-			Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+			googleMap.clear();
 		}
 		return super.onOptionsItemSelected(item);
 	}
 	
+	@SuppressLint("ValidFragment")
 	class MapTypeDialog extends DialogFragment{
 		final String mapTypeKeys[] = {standard, hybrid, political, physical, none};
 		final int mapTypeValues[] = {GoogleMap.MAP_TYPE_NORMAL, 
@@ -155,7 +160,7 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			ListView lv = new ListView(getActivity());
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplication(), android.R.layout.simple_list_item_2, mapTypeKeys);
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplication(), android.R.layout.simple_selectable_list_item, mapTypeKeys);
 			lv.setAdapter(adapter);
 			lv.setOnItemClickListener(new OnItemClickListener() {
 				@Override
@@ -184,4 +189,31 @@ public class MainActivity extends FragmentActivity implements OnMapClickListener
 		textInfo.setVisibility(View.VISIBLE);
 		textInfo.setText(addressLine1 + "\n" + addressLine2 + "\n" + addressLine3);
 	}
+
+	@Override
+	public void onMapLongClick(LatLng ll) {
+		if (markerCounter < maxMarkersCount) {
+			MarkerOptions marker = new MarkerOptions().position(ll).title(markerNames[markerCounter]);
+			llList.add(ll);
+			markerCounter++;
+			googleMap.addMarker(marker);
+		}
+		else{
+			Toast.makeText(this, "No more markers left!", Toast.LENGTH_LONG).show();
+		}
+		if (markerCounter > 1){
+			drawPath(llList);
+		}
+	}
+
+	private void drawPath(List<LatLng> points) {
+		int count = points.size();
+		PolylineOptions polyLine = new PolylineOptions();
+		polyLine.color(Color.BLUE);
+		for (int i = 0; i < count; i++){
+			polyLine.add(points.get(i));
+		}
+		polyLine.geodesic(true);
+		googleMap.addPolyline(polyLine);
+	}	
 }
